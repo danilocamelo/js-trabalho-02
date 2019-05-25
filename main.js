21bd154df3a7fe23bbeeb42eda11487cfeaf24b1
Danilo Machado Alexandre <danilo.alexandre182@gmail.com>
2019-05-25 15:35:35 +0000

    testes sem sucesso

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function montarPagina(pagina){
     var parser = new DOMParser();
     var documento = parser.parseFromString(pagina, "text/html");
-    documento.querySelectorAll("#noticias").forEach(function(div){
+    documento.querySelectorAll(".noticias-direita").forEach(function(div){
         var as = div.children;
         Array.from(as).forEach(function(a){
             var noticia = a.children;
@@ -10,11 +10,27 @@ function montarPagina(pagina){
         //var div2 = document.createElement("div");
         //div2.innerHTML = p;
         //document.body.appendChild(div2);
-    });   
+    });
+}
+
+
+
+// TESTE - DANILO
+function montarPagina1(pagina){
+    var parser = new DOMParser();
+    var documento = parser.parseFromString(pagina, "text/html");
+    documento.querySelectorAll(".grid_8").forEach(function(div){
+        alert("1 forEach");
+        var as = div;
+        Array.from(as).forEach(function(a){
+            alert("1 array");
+            var noticia = a.children;
+            Array.from(noticia).forEach(x => document.body.appendChild(x));
+        })
         //var div2 = document.createElement("div");
         //div2.innerHTML = p;
         //document.body.appendChild(div2);
-    
+    });
 }
 
 //description-noticia
@@ -22,11 +38,13 @@ function sendReq() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
+        //montarPagina1(this.responseText);
         montarPagina(this.responseText);
         alert("OI");
       }
     };
-  
+    
+  //https://yesodaula-romefeller.c9users.io/url/s/www.sosma.org.br
   xhttp.open("GET", "https://yesodaula-romefeller.c9users.io/url/s/www.sosma.org.br", true);
   xhttp.send();
 }
